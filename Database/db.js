5b5583f69b2051f4938044d11f34d8e762f09082
youssufeltahan <117551034+youssufeltahan@users.noreply.github.com>
2023-12-23 17:23:11 +0200

    Update db.js

--- Database/db.js
+++ Database/db.js
@@ -1,8 +1,5 @@
 // Initialize script for creating a database and a collection
-db = db.getSiblingDB('mydatabase'); // Replace 'mydatabase' with your database name
+let db = db.getSiblingDB('admin');
+db = db.getSiblingDB('clinic'); // Replace 'mydatabase' with your database name
 
-// Create a collection and insert a document (optional)
-db.myCollection.insertOne({ 
-    key: 'value',
-    // Other fields...
-});
+
